c97336aea2531c87606c05d86257a80477d6a087
Victor Henrique Dias <101878978+vhdtv@users.noreply.github.com>
2025-01-15 11:08:47 -0300

    Add files via upload

---
+++ gerenciador-de-veiculos/gerenciador-de-veiculos/src/main/java/com/vhdtv/gerenciador_de_veiculos/GerenciadorDeVeiculosApplication.java
@@ -0,0 +1,13 @@
+package com.vhdtv.gerenciador_de_veiculos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorDeVeiculosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorDeVeiculosApplication.class, args);
+	}
+
+}
---
+++ gerenciador-de-veiculos/gerenciador-de-veiculos/src/test/java/com/vhdtv/gerenciador_de_veiculos/GerenciadorDeVeiculosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vhdtv.gerenciador_de_veiculos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorDeVeiculosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
